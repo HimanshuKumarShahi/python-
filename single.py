d1177fb487e8c83f986ffe6dfaadb16efd5c44e8
Himanshu kumar <kh419368@gmail.com>
2025-08-19 23:45:10 +0530

    Add initial project configuration files and Python script with sample code

---
+++ single.py
@@ -0,0 +1,25 @@
+import os
+# from first import *
+# print("hello world");
+
+# print("Enter a");
+# a=int(input());
+# print("Enter b");
+# b=int(input());
+# print("sum =", no1 + no2);
+
+# def sum(a,b):
+#     print("sum=",a +b);
+
+# sum(455,48);
+
+# for c in "hello_world":
+#     print(c);
+
+# for i in range(1,100):
+#     print(i);
+#     if(i%2==0):
+#         print("even number",i)
+#     else:
+#         print("odd number",i)
+os.getcwd();
