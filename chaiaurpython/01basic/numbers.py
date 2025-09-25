c986f3fcf6776258601590ec97a0c80fd3d3a712
Himanshu kumar <himanshushahi2006@gmail.com>
2025-09-25 20:49:50 +0530

    working of numbers

---
+++ chaiaurpython/01basic/numbers.py
@@ -0,0 +1,63 @@
+import math
+import random
+x=2
+y=3
+z=4
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x%y)
+# print((x+y)*z)
+# print(23+44.763)
+# print(int(23+44.763))
+# print(float(23+44.763))
+# # overloading
+# print("chai" + "code")
+# print(x,y,z)
+# print(type(x))
+# print(y%2)
+# print(z ** 4)
+# print(100 ** 2)
+# print(2 ** 100)
+# print(2 ** 10000)
+
+
+# print(1/3)
+# print(1 == 1)
+# print(1.78 != 1)
+
+# print(x<y<z)
+# print(x>y and y<z)
+# print(x<y and y<z)
+# print(x<y or y<z)
+
+# print(math.floor(4.5))
+# print(math.floor(4.99))
+# print(math.trunc(1.99))
+# print(math.trunc(-4.99))
+
+# print((2 + 3j) * 3)
+
+# print(0o157)
+# print(0x157)
+# print(0xf)
+ 
+# print(oct(157))
+# print(hex(157))
+# print(bin(157))
+
+# print(int("157",8))
+# print(int("157",16))
+# print(int("1000",2))
+
+
+# print(random.random())
+# print(random.randint(1,10))
+# print(random.randrange(1,10))
+
+# l1=["hello","world","int","float","string"]
+# print(random.choice(l1))
+# random.shuffle(l1)
+# print(l1)
+
