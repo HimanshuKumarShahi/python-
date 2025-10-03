3c8055616781cb10586583dc1c783410ea52d587
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-10-03 20:20:59 +0530

    learn working of files

---
+++ chaiaurpython/01basic/string.py
@@ -0,0 +1,33 @@
+# chai="massala chai"
+# first_char=chai[4]
+# slice_char=chai[0:6]
+# slice_char=chai[7:12]
+# print(first_char)
+# print(slice_char)
+# print(chai.lower())
+# print(chai.upper())
+# print(chai.replace("chai","powder"))
+
+
+# newchar="   hello   Dear   "
+# print(newchar.strip())
+
+
+# words="lemon,ginger,mint,coffee,juice"
+# print(words)
+# print(words.split())
+# print(words.find("mint"))
+# print(words.count("mint"))
+
+# chai_type="Mint tea"
+# Quantity=4
+# order="i ordered {} cups & {}"
+# print(order)
+# print(order.format(Quantity,chai_type))
+
+# for i in chai:
+#     print(i)
+
+# chai="we are \"indian\""
+# print(chai)
+
