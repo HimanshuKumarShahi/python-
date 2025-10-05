4919b2331cf8c1a6256862dff1993cd68098ccf3
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-10-05 19:46:40 +0530

    working of tuples

---
+++ chaiaurpython/01basic/tuple.py
@@ -0,0 +1,17 @@
+tea_types=("Black","green","Oolang")
+# print(tea_types)
+# print(tea_types[0])
+# print(tea_types[-1])
+# print(tea_types[0:])
+
+coffee_type=("black","brown","white")
+
+all_types=tea_types + coffee_type
+print(all_types)
+if"green"in all_types:
+    print("present here")
+
+(black,green,Oolang)=tea_types
+print(black)
+
+print(type(all_types))
