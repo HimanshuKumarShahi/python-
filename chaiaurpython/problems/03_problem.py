3c33b3fe08e4e78b87e62421df1e057c2f9e80cb
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-10-05 20:56:17 +0530

    grade calculator with grade

--- chaiaurpython/problems/03_problem.py
+++ chaiaurpython/problems/03_problem.py
@@ -6,22 +6,22 @@ grade=str(input("assign GRADE in captial letter b/w A to E "))
 if(grade=="A"):
     print("Student Name:",name)
     print("Student Score:",score)
-    print("your score is nice and b/w 90-100")
+    print("your score is nice and b/w 90-100 grade=",grade)
 elif(grade=="B"):
     print("Student Name:",name)
     print("Student Score:",score)
-    print("your score is good and b/w 80-89")
+    print("your score is good and b/w 80-89 grade=",grade)
 elif(grade=="C"):
     print("Student Name:",name)
     print("Student Score:",score)
-    print("your score is average and b/w 70-79")
+    print("your score is average and b/w 70-79 grade=",grade)
 elif(grade=="D"):
     print("Student Name:",name)
     print("Student Score:",score)
-    print("your score is below average and b/w 60-69")
+    print("your score is below average and b/w 60-69 grade=",grade)
 elif(grade=="E"):
     print("Student Name:",name)
     print("Student Score:",score)
-    print("your score is poor and below 60")
+    print("your score is poor and below 60 grade=",grade)
 else:
     print("please enter valid input !!")
